e5fbd8f431d629086c05faccf7ef80c65154ba57
scott weiss <sdw35@cornell.edu>
2019-04-26 00:36:10 -0400

    add to test
    Signed-off-by: scott weiss <sdw35@cornell.edu>

--- pkg/api/v1/clients/wrapper/multi_watcher_test.go
+++ pkg/api/v1/clients/wrapper/multi_watcher_test.go
@@ -15,18 +15,21 @@ import (
 )
 
 var _ = Describe("watchAggregator", func() {
-	var cluster1, cluster2, cluster3 *Client // add / remove later
+	var cluster1, cluster2, cluster3 , cluster4 *Client // add / remove later
 	var watcher WatchAggregator
 	clusterName1 := "clustr1"
 	clusterName2 := "clustr2"
 	clusterName3 := "clustr3"
+	clusterName4 := "clustr4"
 	BeforeEach(func() {
 		base1 := memory.NewResourceClient(memory.NewInMemoryResourceCache(), &v1.MockResource{})
 		base2 := memory.NewResourceClient(memory.NewInMemoryResourceCache(), &v1.MockResource{})
 		base3 := memory.NewResourceClient(memory.NewInMemoryResourceCache(), &v1.MockResource{})
+		base4 := memory.NewResourceClient(memory.NewInMemoryResourceCache(), &v1.MockResource{})
 		cluster1 = NewClusterClient(base1, clusterName1)
 		cluster2 = NewClusterClient(base2, clusterName2)
 		cluster3 = NewClusterClient(base3, clusterName3)
+		cluster4 = NewClusterClient(base4, clusterName4)
 
 		watcher = NewWatchAggregator()
 		err := watcher.AddWatch(cluster1)
@@ -37,12 +40,9 @@ var _ = Describe("watchAggregator", func() {
 	It("aggregates watches from multiple clients", func() {
 		watch, errs, err := watcher.Watch("", clients.WatchOpts{RefreshRate: time.Millisecond})
 		Expect(err).NotTo(HaveOccurred())
-		wait := make(chan struct{})
+
 		go func() {
 			defer GinkgoRecover()
-			defer func() {
-				close(wait)
-			}()
 			_, err = cluster1.Write(v1.NewMockResource("a", "a"), clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 			_, err = cluster1.Write(v1.NewMockResource("a", "b"), clients.WriteOpts{})
@@ -61,11 +61,6 @@ var _ = Describe("watchAggregator", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 		}()
-		select {
-		case <-wait:
-		case <-time.After(time.Second * 5):
-			Fail("expected wait to be closed before 5s")
-		}
 
 		var list resources.ResourceList
 
@@ -80,7 +75,7 @@ var _ = Describe("watchAggregator", func() {
 				Fail("expected a message in channel")
 			}
 			return nil
-		}, time.Second*200).Should(HaveLen(6))
+		}, time.Second*10).Should(HaveLen(6))
 
 		list.Each(util.ZeroResourceVersion)
 
@@ -92,5 +87,40 @@ var _ = Describe("watchAggregator", func() {
 			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "a", Cluster: "clustr3"}},
 			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "b", Cluster: "clustr3"}},
 		}))
+
+
+		go func() {
+			watcher.RemoveWatch(cluster3)
+			err = watcher.AddWatch(cluster4)
+			Expect(err).NotTo(HaveOccurred())
+
+			// update
+			_, err = cluster4.Write(v1.NewMockResource("a", "b"), clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+		}()
+
+		Eventually(func() resources.ResourceList {
+			select {
+			default:
+			case err := <-errs:
+				Expect(err).NotTo(HaveOccurred())
+			case list = <-watch:
+				return list
+			case <-time.After(time.Millisecond * 5):
+				Fail("expected a message in channel")
+			}
+			return nil
+		}, time.Second*10).Should(HaveLen(5))
+
+		list.Each(util.ZeroResourceVersion)
+
+		Expect(list).To(Equal(resources.ResourceList{
+			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "a", Cluster: "clustr1"}},
+			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "b", Cluster: "clustr1"}},
+			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "a", Cluster: "clustr2"}},
+			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "b", Cluster: "clustr2"}},
+			&v1.MockResource{Metadata: core.Metadata{Namespace: "a", Name: "b", Cluster: "clustr4"}},
+		}))
+
 	})
 })
