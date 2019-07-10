9d7687d826ac77fe94fde4cc63967d33d1bb897f
Joe Kelley <jsphklly@gmail.com>
2019-07-10 09:56:07 -0400

    redo

--- test/mocks/JUNK_mock_resource_converter.go
+++ test/mocks/JUNK_mock_resource_converter.go
@@ -3,20 +3,19 @@ package mocks
 import (
 	"github.com/solo-io/go-utils/versionutils/kubeapi"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube/crd"
-	"github.com/solo-io/solo-kit/pkg/errors"
 	v1 "github.com/solo-io/solo-kit/test/mocks/v1"
 	"github.com/solo-io/solo-kit/test/mocks/v1alpha1"
 	"github.com/solo-io/solo-kit/test/mocks/v2alpha1"
 )
 
 type UpConverter interface {
-	FromV1Alpha1ToV1(src *v1alpha1.MockResource, dst *v1.MockResource) error
-	FromV1ToV2Alpha1(src *v1.MockResource, dst *v2alpha1.MockResource) error
+	FromV1Alpha1ToV1(src *v1alpha1.MockResource) *v1.MockResource
+	FromV1ToV2Alpha1(src *v1.MockResource) *v2alpha1.MockResource
 }
 
 type DownConverter interface {
-	FromV2Alpha1ToV1(src *v2alpha1.MockResource, dst *v1.MockResource) error
-	FromV1ToV1Alpha1(src *v1.MockResource, dst *v1alpha1.MockResource) error
+	FromV2Alpha1ToV1(src *v2alpha1.MockResource) *v1.MockResource
+	FromV1ToV1Alpha1(src *v1.MockResource) *v1alpha1.MockResource
 }
 
 type MockResourceConverter struct {
@@ -29,47 +28,49 @@ func (c *MockResourceConverter) Convert(src, dst crd.SoloKitCrd) error {
 	if err != nil {
 		return err
 	}
-
 	dstVersion, err := kubeapi.ParseVersion(dst.GetObjectKind().GroupVersionKind().Version)
 	if err != nil {
 		return err
 	}
 
 	if srcVersion.GreaterThan(dstVersion) {
-		switch src.GetObjectKind().GroupVersionKind().Version {
-		case "v2alpha1":
-			err = c.DownConverter.FromV2Alpha1ToV1(src.(*v2alpha1.MockResource), dst.(*v1.MockResource))
-			if err != nil {
-				return err
-			}
-		case "v1":
-			err = c.DownConverter.FromV1ToV1Alpha1(src.(*v1.MockResource), dst.(*v1alpha1.MockResource))
-			if err != nil {
-				return err
-			}
-		default:
-			return errors.Errorf("Cannot convert up from %v", src.GetObjectKind().GroupVersionKind().Version)
-		}
-		return c.Convert(src, dst)
+		c.convertDown(src, dst)
 	}
-
 	if srcVersion.LessThan(dstVersion) {
-		switch src.GetObjectKind().GroupVersionKind().Version {
-		case "v1alpha1":
-			err = c.UpConverter.FromV1Alpha1ToV1(src.(*v1alpha1.MockResource), dst.(*v1.MockResource))
-			if err != nil {
-				return err
-			}
-		case "v1":
-			err = c.UpConverter.FromV1ToV2Alpha1(src.(*v1.MockResource), dst.(*v2alpha1.MockResource))
-			if err != nil {
-				return err
-			}
-		default:
-			return errors.Errorf("Cannot convert up from %v", src.GetObjectKind().GroupVersionKind().Version)
-		}
-		return c.Convert(src, dst)
+		c.convertUp(src, dst)
 	}
 
 	return nil
+}
+
+func (c *MockResourceConverter) convertDown(src, dst crd.SoloKitCrd) {
+	if src.GetObjectKind().GroupVersionKind().Version == dst.GetObjectKind().GroupVersionKind().Version {
+		return
+	}
+
+	switch src.GetObjectKind().GroupVersionKind().Version {
+	case "v2alpha1":
+		src = c.DownConverter.FromV2Alpha1ToV1(src.(*v2alpha1.MockResource))
+	case "v1":
+		src = c.DownConverter.FromV1ToV1Alpha1(src.(*v1.MockResource))
+	default:
+		return
+	}
+	c.convertDown(src, dst)
+}
+
+func (c *MockResourceConverter) convertUp(src, dst crd.SoloKitCrd) {
+	if src.GetObjectKind().GroupVersionKind().Version == dst.GetObjectKind().GroupVersionKind().Version {
+		return
+	}
+
+	switch src.GetObjectKind().GroupVersionKind().Version {
+	case "v1alpha1":
+		src = c.UpConverter.FromV1Alpha1ToV1(src.(*v1alpha1.MockResource))
+	case "v1":
+		src = c.UpConverter.FromV1ToV2Alpha1(src.(*v1.MockResource))
+	default:
+		return
+	}
+	c.convertUp(src, dst)
 }
